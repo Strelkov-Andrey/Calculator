a10ab52fcacd1b3b1ce4298f9156e521e8688c30
Strelkov-Andrey <62470514+Strelkov-Andrey@users.noreply.github.com>
2026-06-05 15:47:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+let sbp = [0.7,0.4,0.2];
+let digital_rub = [0.3,0.2];
+let equiring = 2.5;
+
+//var sum = 100000;
+
+var res_sbp;
+var res_dr;
+var res_eq;
+
+//const type = 4.1;
+//const period = 3;
+
+
+document
+    .getElementById('calculatorForm')
+    .addEventListener('submit', function(e) {
+
+        e.preventDefault();
+
+        const type = Number(
+            document.querySelector(
+                'input[name="type"]:checked'
+            ).value
+        );
+        const period = Number(
+            document.querySelector(
+                'input[name="period"]:checked'
+            ).value
+        );
+        const sum = Number(document.getElementById('amount').value);
+
+        const result = calculate(
+            type,
+            period,
+            sum
+        );
+
+        localStorage.setItem(
+    'calcResult',
+    JSON.stringify(result)
+);
+
+        window.location.href = 'result.html';
+    });
+function calculate(type, period, sum){
+    switch (type) {
+    case 2.1:
+    case 2.2:
+    case 2.3:
+    case 2.4:
+    case 2.5:
+    case 2.6:
+    case 2.7:
+    case 2.8:
+    case 2.9:
+        res_sbp = period * (sum * sbp[1] / 100),
+        res_dr = period * (sum * digital_rub[0] / 100),
+        res_eq = period * (sum * equiring / 100)
+        return {res_sbp,res_dr,res_eq};
+
+    case 3.1:
+        res_sbp = period * (sum * sbp[0] / 100),
+        res_dr = period * (sum * digital_rub[0] / 100),
+        res_eq = period * (sum * equiring / 100)
+        return {res_sbp,res_dr,res_eq};
+
+    case 4.1:
+        res_sbp = period * (sum * sbp[2] / 100),
+        res_dr = period * (sum * digital_rub[1] / 100),
+        res_eq = period * (sum * equiring / 100)
+        return {res_sbp,res_dr,res_eq};
+  default:
+    console.log("Error");
+}
+}
